015158115e59b04fb5ede9c78b10ed3a96051dec
Varma Cephas <varmac231@gmail.com>
2025-03-17 21:09:11 +0200

    feat: Add navigation bar and made responsive

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    const hamburger_btn = document.getElementById("hamburger_btn");
+    const hamburger_state = document.getElementById("hamburger-state");
+    const hamburger_x_btn = document.getElementById("hamburger_x_btn");
+    hamburger_btn.addEventListener("click", ()=>{
+        hamburger_btn.style.display="none";
+        hamburger_x_btn.style.display="block";
+    })
+
+    hamburger_x_btn.addEventListener("click", ()=>{
+        hamburger_btn.style.display="block";
+        hamburger_x_btn.style.display="none";
+    })
+
+},200)
