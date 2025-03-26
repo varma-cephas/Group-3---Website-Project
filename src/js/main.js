eddb44d29d619202629ae40b9de7a22bb0eeb67f
Varma Cephas <varmac231@gmail.com>
2025-03-26 11:58:36 +0200

    update: Implement sticky header, fix link path to customer section

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,14 @@ setTimeout(()=>{
    const hamburger_btn = document.getElementById("hamburger_btn");
     const hamburger_x_btn = document.getElementById("hamburger_x_btn");
     const mobile_nav = document.getElementById("mobile_nav");
-    const hero_section = document.getElementById("hero")
+    const hero_section = document.getElementById("hero");
+    const nav_header = document.getElementById("nav");
 
     hamburger_btn.addEventListener("click", ()=>{
         hamburger_btn.style.display="none";
         hamburger_x_btn.style.display="block";
         mobile_nav.style.display="block";
+        nav_header.style.backgroundColor="white";
         hero_section.style.display="none"
     })
     
@@ -15,6 +17,7 @@ setTimeout(()=>{
         hamburger_btn.style.display="block";
         hamburger_x_btn.style.display="none";
         mobile_nav.style.display="none";
+        nav_header.style.backgroundColor="transparent";
         hero_section.style.display="block";
     })
 
