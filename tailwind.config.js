4f40b00ffd1d746257f8b8edb06e365def08a263
karbecca <karbecca3@gmail.com>
2025-04-03 09:03:23 +0200

    add assets folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#00672E",
+      },
+      width: {
+        "custom-sm": "20.094rem",
+        "custom-md": "24.094rem",
+        "custom-lg": "26.094rem",
+      },
+      height: {
+        "custom-sm": "10.125rem",
+        "custom-md": "12.125rem",
+        "custom-lg": "15.125rem",
+      },
+    },
   },
   plugins: [],
-}
+};
